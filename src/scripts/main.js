08d909ff45a4e451d6f31ae6df8dd94e09d26015
Taras <hevko.tars@gmail.com>
2021-06-16 12:25:02 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const burger = document.querySelector('.header__burger');
-const links = document.querySelector('.header__nav');
-
-burger.addEventListener('click', () => {
-  links.classList.toggle('header__nav--active');
-  burger.classList.toggle('header__burger--active');
-});
